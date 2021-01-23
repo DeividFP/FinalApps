7341db0657517f7728bfd903b8fa9474d108d0d9
David <farelasdavid.98@gmail.com>
2021-01-23 16:29:47 -0600

    Primera version

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const multer = require('multer');
+
+app.get("/", (req, res)=>{
+    console.log(__dirname);
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+io.sockets.on("connection", (socket) => {
+    socket.on("username", (username) => {
+        socket.username =  username;
+        io.emit("is_online", `${username} se unio al chat`);
+    });
+    socket.on('chat_message', (message) => {
+        io.emit('chat_message', `<strong> ${socket.username}: </strong> ${message}`)
+    });
+});
+
+const server = http.listen(8080, () => {
+    console.log("Server started on port 8080")
+});

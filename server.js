92f0e9416cfcc11c57140a16e90c91a8c8ed14ac
Luis Fernando Bermudez <72872597+LFBA141@users.noreply.github.com>
2021-01-23 22:57:14 -0600

    modificaciones server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const multer = require('multer');
 
+
 app.get("/", (req, res)=>{
     console.log(__dirname);
     res.sendFile(__dirname + "/views/index.html");
@@ -17,8 +18,13 @@ io.sockets.on("connection", (socket) => {
     socket.on('chat_message', (message) => {
         io.emit('chat_message', `<strong> ${socket.username}: </strong> ${message}`)
     });
+    socket.on('upload_image', (img)=>{
+        //insertar img en BD 
+        
+        io.emit('uploaded_img', `<strong> ${socket.username}: </strong>`, img);
+    });
 });
 
 const server = http.listen(8080, () => {
-    console.log("Server started on port 8080")
+    console.log("Server started on port 8080");
 });
